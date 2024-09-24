d3f373c34b310e422660a90feb97da586a9dd6f4
lfer9980 <luis_fernandez99@hotmail.com>
2024-09-24 11:39:12 -0600

    🎨 WIP: python course

--- day_5/main.py
+++ day_5/main.py
@@ -1,14 +1,6 @@
 import random
 import string
 
-
-print("Welcome to the PyPassword Generator!")
-# nr_letters= int(input("How many letters would you like in your password?\n")) 
-# nr_symbols = int(input(f"How many symbols would you like?\n"))
-# nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-# new_list: list = []
-
 class PasswordGen: 
     letters: list = list(string.ascii_letters)
     numbers: list = list(string.digits)
@@ -16,35 +8,43 @@ class PasswordGen:
     total_data: list = [*letters, *numbers, *symbols]
     
     @classmethod
-    def _random_(cls) -> str:
-        return random.choice(cls.total_data)
+    def _random_(self, data: list, n:int) -> str:
+        return random.choices(data, k=n)
     
     @classmethod
-    def create_password(cls, len_pss: int) -> str:
-        pss: list = [PasswordGen._random_() for _ in range(len_pss)]
+    def create_password(self, len_letters: int, len_symbols: int, len_numbers: int, is_shuffle: bool) -> str:
+        pss: list = []
+        
+        pss.extend(PasswordGen._random_(data=self.letters, n=len_letters))
+        pss.extend(PasswordGen._random_(data=self.numbers, n=len_symbols))
+        pss.extend(PasswordGen._random_(data=self.symbols, n=len_numbers))
+        
+        if (is_shuffle): random.shuffle(pss)
         
         return ''.join(pss)
     
-    def __call__(self, len_pss: int) -> str:
-        return self.create_password(len_pss=len_pss)
+    def __call__(self, len_letters: int, len_symbols: int, len_numbers: int, is_shuffle: int) -> str:
+        
+        is_shuffle: bool = True if is_shuffle is 1 else False
+        
+        return self.create_password(len_letters=len_letters,
+                                    len_symbols=len_symbols,
+                                    len_numbers=len_numbers,
+                                    is_shuffle=is_shuffle)
 
-	# @staticmethod
-	# def select_random_item(base_list:list, len_new_list: int) -> list:     
-	# 	len_base_list: int = len(base_list)
-  
-	# 	return [base_list[PasswordGen._random_number(len_base_list)] for _ in range(0, len_new_list)]
-
-	# @staticmethod
-	# def create_password(data: list) -> str:
-	# 	random.shuffle(data)
-	# 	return ''.join(data)
- 	
 
 if __name__ == "__main__":
     
-    # final_list: str = [*letters_list, *symbols_list, *numbers_list]
-    # # new_list:str = ''.join([*letters_list, *symbols_list, *numbers_list])
+    print("Welcome to the PyPassword Generator!")
     
-    # print(new_list)
-    print(PasswordGen()(100))
+    nr_letters= int(input("How many letters would you like in your password?\n")) 
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+    shuffle = int(input(f"Do you like shuffle the password? 1 for true, 0 for false\n"))
     
+    
+    print(PasswordGen()(len_letters=nr_letters,
+                        len_symbols=nr_symbols, 
+                        len_numbers=nr_numbers,
+                        is_shuffle=shuffle)
+          )
